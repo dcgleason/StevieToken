6e8f67da50fe2e9acfcc0d262b6f81eb7012efd8
Dan Gleason <dannycgleason@gmail.com>
2021-05-07 08:42:35 -0400

    Add files via upload

---
+++ truffle-config.js
@@ -0,0 +1,32 @@
+module.exports = {
+  // See <http://truffleframework.com/docs/advanced/configuration>
+  // to customize your Truffle configuration!
+  networks: {
+    development: {
+      host: "127.0.0.1",
+      port: "7545",
+      network_id: "*" // match any network id
+    },
+    rinkeby: {
+      host: "localhost",
+      port: 8545,
+      network_id: 4,
+      gas: 4700000
+    }
+  },
+  compilers: {
+    solc: {
+      version: "^0.5.17", // A version or constraint - Ex. "^0.5.0"
+                         // Can also be set to "native" to use a native solc
+      docker: false, // Use a version obtained through docker
+      parser: "solcjs",  // Leverages solc-js purely for speedy parsing
+      settings: {
+        optimizer: {
+          enabled: true,
+          runs: 1   // Optimize for how many times you intend to run the code
+        },
+        evmVersion: "istanbul" // Default: "istanbul"
+      },
+    }
+  }
+};
